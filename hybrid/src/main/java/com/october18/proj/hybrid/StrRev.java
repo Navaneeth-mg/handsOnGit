92716d967b5f5595a71d4db82c7e0986eb445ae1
Navaneeth <navamg09@in.ibm.com>
2018-12-26 13:55:30 +0530

    Testing Pull request from eclipse.

---
+++ hybrid/src/main/java/com/october18/proj/hybrid/StrRev.java
@@ -0,0 +1,33 @@
+package com.october18.proj.hybrid;
+
+public class StrRev {
+	
+	
+	
+	static String recursiveMtd(String str1) {
+		
+		if(null == str1||str1.length()<=0) {
+			
+			return str1;
+		}else {
+			
+			return recursiveMtd(str1.substring(1)) + str1.charAt(0);
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str = "abcd";
+		
+		char[] ch = str.toCharArray();
+		//System.out.println(ch.length);
+		
+		for(int i=ch.length-1;i>=0;i--){
+		System.out.print(ch[i]);
+	}
+		System.out.println();
+		System.out.print(StrRev.recursiveMtd("David"));
+}
+
+}
